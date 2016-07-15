94581c5c55cd554d4e9003f76a63a593db63725e
Andrew Lopreiato <drew.lopreiato@gmail.com>
2016-07-14 20:10:31 -0400

    Implement string escaping in add exercise utility

--- util/Add-Exercise.php
+++ util/Add-Exercise.php
@@ -12,7 +12,7 @@ if (isset($_POST['exercisename']))
     $successfullyAdded = false;
 
     $insertExerciseQuery = 'INSERT INTO exercises (`name`) VALUES (\'%1$s\')';
-    if ($databaseConnection->query(sprintf($insertExerciseQuery, $_POST['exercisename'])))
+    if ($databaseConnection->query(sprintf($insertExerciseQuery, $databaseConnection->real_escape_string($_POST['exercisename']))))
     {
        $successfullyAdded = true;
     }
